dda6c3d9b8fd20bce3b1bd171e1b823efe0e2f12
Colin Mudie <csmudie1@gmail.com>
2021-12-31 17:33:50 -0500

    added auth0 & bootswatch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      
+    ]),
+    AuthModule.forRoot({
+      domain: 'dev-x9fipz-q.us.auth0.com',
+      clientId: '3JAzKg2MnGSVzCVXoqr3zV4ReFUrZc52',
+      cacheLocation: 'localstorage',
+      useRefreshTokens: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
