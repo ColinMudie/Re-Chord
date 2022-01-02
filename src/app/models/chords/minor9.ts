4dfcb32fb66b77fff0f3e7652909a31467d8160d
Colin Mudie <csmudie1@gmail.com>
2022-01-01 19:08:23 -0500

    created minor9 chord class

---
+++ src/app/models/chords/minor9.ts
@@ -0,0 +1,64 @@
+import { Chord } from "../chord"
+
+export class Minor9
+{
+    public C: Chord = {
+            notes: ['C ', ' G ', 'Bb ', 'D ', 'Eb ', 'G '],
+            name: 'C min9'
+        };
+
+    Db: Chord = {
+        notes: ['Db ', 'Ab ', 'B ', 'Eb ', 'E ', 'Ab '],
+        name: 'Db min9'
+    };
+
+    D: Chord = {
+        notes: ['D ', 'A ', 'C ', 'E ', 'F ', 'A '],
+        name: 'D min9'
+    };
+
+    Eb: Chord = {
+        notes: ['Eb ', 'Bb ', 'Db ', 'F ', 'Gb ', 'Bb '],
+        name: 'Eb min9'
+    };
+
+    E: Chord = {
+        notes: ['E ', 'B ', 'D ', 'Gb ', 'G ', 'B '],
+        name: 'E min9'
+    };
+
+    F: Chord = {
+        notes: ['F ', 'C ', 'Eb ', 'G ', 'Ab ', 'C '],
+        name: 'F min9'
+    };
+
+    Gb: Chord = {
+        notes: ['Gb ', 'Db ', 'E ', 'Ab ', 'A ', 'Db '],
+        name: 'Gb min9'
+    };
+
+    G: Chord = {
+        notes: ['G ', 'D ', 'F ', 'A ', 'Bb ', 'D '],
+        name: 'G min9'
+    };
+
+    Ab: Chord = {
+        notes: ['Ab ', 'Eb ', 'Gb ', 'Bb ', 'B ', 'Eb '],
+        name: 'Ab min9'
+    };
+
+    A: Chord = {
+        notes: ['A ', 'E ', 'G ', 'B ', 'C ', 'E '],
+        name: 'A min9'
+    };
+
+    Bb: Chord = {
+        notes: ['Bb ', 'F ', 'Ab ', 'C ', 'Db ', 'F '],
+        name: 'Bb min9'
+    };
+
+    B: Chord = {
+        notes: ['B ', 'Gb ', 'A ', 'Db ', 'D ', 'Gb '],
+        name: 'B min9'
+    }
+}
