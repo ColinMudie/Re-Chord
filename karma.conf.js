069bb70d7376e878d29c5d4056bdee9c3447a317
Colin Mudie <csmudie1@gmail.com>
2022-01-01 21:37:29 -0500

    renamed angular app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/MIDI'),
+      dir: require('path').join(__dirname, './coverage/Re-Chord'),
       subdir: '.',
       reporters: [
         { type: 'html' },
